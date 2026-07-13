e7981431f92b369df0e5862c42a61a3b81334d3d
vanshi-21ka <vermavanshika000@gmail.com>
2026-07-13 18:37:23 +0530

    remove: delete ambulance routes and references

--- healthcare-platform/backend/venv/Scripts/activate.bat
+++ healthcare-platform/backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sushm\Downloads\healthcare-platform\healthcare-platform\backend\venv
+set VIRTUAL_ENV=C:\Users\verma\Health_care_management\healthcare-platform\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
